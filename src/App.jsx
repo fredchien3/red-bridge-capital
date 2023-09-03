9966223aba32e48e49ee67ebe402d01889a824ae
Fred Chien <fred.chien3@gmail.com>
2023-09-02 22:13:47 -0400

    Add about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import AboutSection from "./components/AboutSection"
 import IntroSection from "./components/IntroSection"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <div className="w-full h-screen overflow-y-auto overflow-x-hidden" style={{ perspective: '15px' }}>
       <IntroSection />
+      <AboutSection />
       <footer className="bg-white w-full h-[1000px]">
         foot
       </footer>
--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -1,15 +1,17 @@
 import wall from "../assets/images/wall.jpg";
-import ifc from "../assets/images/ifc.jpg";
 
 export default function IntroSection() {
   return (
     <>
-      <header className="w-full h-2/3 relative flex justify-center items-center -z-10" style={{ transformStyle: 'preserve-3d' }}>
-        <div className="w-full h-full absolute object-cover -z-10" style={{ backgroundImage: `url(${wall})`, backgroundPosition: 'center', backgroundSize: 'cover', transform: 'translateZ(-3px) scale(1.5)'}}>
+      <header className="w-full h-3/5 relative flex justify-center items-center -z-10" style={{ transformStyle: 'preserve-3d' }}>
+        <div
+          className="w-full h-full absolute object-cover -z-10"
+          style={{ backgroundImage: `url(${wall})`, backgroundPosition: 'center',
+          backgroundSize: 'cover', transform: 'translateZ(-4px) scale(1.5)'}}>
         </div>
         <h1 className="absolute left-16 bottom-16 text-8xl uppercase text-white">立足香港 . 背靠嶺南 . 眼向世界</h1>
       </header>
-      <div className="flex justify-between h-1/3 font-ss">
+      <section className="flex justify-between h-2/5 font-ss">
         <aside className="flex flex-col w-1/2 h-full bg-maroon text-white">
           <div className="flex justify-between items-start w-full h-1/2 p-6">
             <ul>
@@ -30,7 +32,7 @@ export default function IntroSection() {
             <li>contact@redbridge.com</li>
           </ul>
         </aside>
-      </div>
+      </section>
     </>
   )
 }
