4c54af615e5153b6d2b542cb63ff79902316b9b6
Fred Chien <fred.chien3@gmail.com>
2023-10-07 17:41:24 -0400

    Refactor intro animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import { ParallaxProvider } from "react-scroll-parallax"
-import Lottie from "lottie-react"
 import AboutSection from "./components/AboutSection/AboutSection"
 import ContactSection from "./components/ContactSection"
 import FooterSection from "./components/FooterSection"
 import IntroSection from "./components/IntroSection/IntroSection"
-import introAnimation from "./assets/rb_logo_animation.json"
-import { useEffect, useRef, useState } from "react"
+import IntroAnimation from "./components/IntroAnimation"
 
 function App() {
 
   return (
     <ParallaxProvider>
       <div className="w-full h-screen font-ss">
-        <AnimationComponent />
+        <IntroAnimation />
         <IntroSection />
         <AboutSection />
         <ContactSection />
@@ -21,45 +19,5 @@ function App() {
     </ParallaxProvider>
   )
 }
-
-const AnimationComponent = () => {
-  const lottieRef = useRef();
-  const [animationState, setAnimationState] = useState(0);
-  
-  useEffect(() => {
-    lottieRef.current.setSpeed(1.3);
-    setTimeout(() => {
-      setAnimationState(1);
-      document.body.classList.remove("noscroll");
-    }, 5500);
-    setTimeout(() => {
-      setAnimationState(2);
-    }, 6500);
-  }, [])
-
-  let animationClass = '';
-  switch (animationState) {
-    case 0:
-      animationClass = 'bg-white';
-      break;
-    case 1:
-      animationClass = 'bg-transparent';
-      break;
-    case 2:
-      animationClass = 'hidden';
-      break;
-  }
-
-  return (
-    <Lottie
-      lottieRef={lottieRef}
-      animationData={introAnimation}
-      loop={false}
-      className={`w-full h-screen fixed z-20 transition duration-1000 noscroll
-      ${animationClass}`}
-    />
-  );
-};
-
 
 export default App
---
+++ src/components/IntroAnimation.jsx
@@ -0,0 +1,43 @@
+import Lottie from "lottie-react"
+import introAnimation from "../assets/rb_logo_animation.json"
+import { useEffect, useRef, useState } from "react"
+
+
+export default function IntroAnimation() {
+  const lottieRef = useRef();
+  const [animationState, setAnimationState] = useState(0);
+  
+  useEffect(() => {
+    lottieRef.current.setSpeed(1.3);
+    setTimeout(() => {
+      setAnimationState(1);
+      document.body.classList.remove("noscroll");
+    }, 5500);
+    setTimeout(() => {
+      setAnimationState(2);
+    }, 6500);
+  }, [])
+
+  let animationClass = '';
+  switch (animationState) {
+    case 0:
+      animationClass = 'bg-white';
+      break;
+    case 1:
+      animationClass = 'bg-transparent';
+      break;
+    case 2:
+      animationClass = 'hidden';
+      break;
+  }
+
+  return (
+    <Lottie
+      lottieRef={lottieRef}
+      animationData={introAnimation}
+      loop={false}
+      className={`w-full h-screen fixed z-20 transition duration-1000 noscroll
+      ${animationClass}`}
+    />
+  );
+};
