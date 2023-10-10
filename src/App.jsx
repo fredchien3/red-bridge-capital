9c862d5afa96e719235c9d6f305b523f35c874dc
Fred Chien <fred.chien3@gmail.com>
2023-10-09 23:16:48 -0400

    Remember language (local) and animation (session)

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,31 @@ import ContactSection from "./components/ContactSection"
 import FooterSection from "./components/FooterSection"
 import IntroSection from "./components/IntroSection/IntroSection"
 import IntroAnimation from "./components/IntroAnimation"
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 
 export const LanguageContext = createContext();
 
 function App() {
-  const [language, setLanguage] = useState(0);
+  const [language, setLanguage] = useState(parseInt(localStorage.getItem("language")) || 0);
   // 0 - Chinese
   // 1 - English
 
+  useEffect(() => {
+    sessionStorage.setItem("visited", "true");
+  }, [])
+
   function toggleLanguage() {
-    setLanguage(prevLanguage => !prevLanguage);
+    const prevLanguage = language;
+    const nextLanguage = prevLanguage === 0 ? 1 : 0;
+    setLanguage(nextLanguage);
+    localStorage.setItem("language", nextLanguage);
   }
   
   return (
     <ParallaxProvider>
       <LanguageContext.Provider value={language}>
         <div className="w-full h-screen font-ss">
-          <IntroAnimation />
+          { sessionStorage.getItem("visited") === "true" ? <></> : <IntroAnimation />}
           <IntroSection toggleLanguage={toggleLanguage}/>
           <AboutSection />
           <ContactSection />
