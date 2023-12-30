1c946abf8858185a7726a26f302c90a5895028d3
Fred Chien <fred.chien3@gmail.com>
2023-12-31 01:16:14 +0800

    Adjust footer spacing + text

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -16,28 +16,31 @@ export default function FooterSection() {
           className="h-full"
         />
       </section>
-      <footer className="w-full h-auto lg:h-2/5 flex flex-col justify-between p-[6vw] 2xl:pb-[18vw] bg-jade text-beige
-      text-[2.7vw] sm:text-[2vw] lg:text-[1.4vw] xl:text-[1.2vw] 2xl:text-[1vw]">
+      <footer className="w-full h-auto flex flex-col justify-between p-[4vw] 2xl:pb-16 bg-jade text-beige
+      text-sm sm:text-base md:text-2xl lg:text-2xl xl:text-xl 2xl:text-2xl">
         <div className="flex justify-end">
-            <div className="flex items-center" onClick={scrollUp}>
-              <p className="mr-4">Back to top</p>
-              <img src={arrowUp} className="floating-arrow h-6 md:h-11" />
-            </div>
+          <div className="flex items-center" onClick={scrollUp}>
+            <p className="mr-4">Back to top</p>
+            <img src={arrowUp} className="floating-arrow h-6 md:h-11" />
+          </div>
         </div>
         <div className="flex justify-between items-end pb-4 md:pb-7 mt-12 md:mt-24">
-          <aside className="flex justify-between flex-col-reverse h-[30vw] lg:h-auto lg:flex-row items-start lg:items-end">
-            <img src={footerLogo} alt="Red Bridge Capital logo" className="h-[16vw] md:h-[8vw] object-contain -mb-2 md:-mb-7 lg:mr-[8vw] 2xl:mr-[12vw]" />
+          <aside className="flex justify-between flex-col-reverse h-[30vw] lg:h-[20vw] xl:h-auto xl:flex-row items-start xl:items-end">
+            <img
+              src={footerLogo}
+              alt="Red Bridge Capital logo"
+              className="h-[16vw] md:h-[8vw] object-contain -mb-2 mt-6 sm:mt-0 md:-mb-7 xl:mr-[8vw] 2xl:mr-[12vw]"
+            />
             <a
-              href="#"
+              href="https://maps.app.goo.gl/V8BcTVr5KPZyvFyFA"
               target="_blank"
-              className="leading-none hover:text-maroon duration-300"
+              className="leading-none hover:text-maroon duration-300 whitespace-pre-wrap"
             >
-              <br />
-              190 Eigin Avenue, George Town,<br />Grand Cayman KY1-9008, Cayman Islands
+              {"190 Eigin Avenue,\nGeorge Town,Grand Cayman\nKY1-9008, Cayman Islands"}
             </a>
           </aside>
-          <aside className="flex justify-between flex-col h-[30vw] lg:h-auto lg:flex-row items-end">
-            <ul className="text-right lg:text-left leading-none lg:mr-[8vw] 2xl:mr-[12vw]">
+          <aside className="flex justify-between flex-col h-[30vw] lg:h-[20vw] xl:h-auto xl:flex-row items-end">
+            <ul className="text-right xl:text-left leading-none xl:mr-[8vw] 2xl:mr-[12vw]">
               <li>Contact</li>
               <li>
                 <a href="tel:+85234695185"
