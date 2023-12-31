b3363343b7886918eccb58468412eb47db6da086
Fred Chien <fred.chien3@gmail.com>
2023-12-31 14:49:37 +0800

    Organize mobile nav bar
    - Adjust about text size

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -33,7 +33,7 @@ export default function AboutSection() {
   }
 
   return (
-    <section id="about-section" className="w-full h-auto xl:h-full flex flex-col xl:flex-row justify-between relative z-10">
+    <section id="about-section" className="w-full h-auto xl:h-[90%] flex flex-col xl:flex-row justify-between relative z-10">
       <Carousel className={"w-full xl:w-1/2 h-96 xl:h-full relative -z-30 overflow-hidden"} />
       <aside
         className={`w-full h-3/4 flex flex-col bg-beige text-slate py-[8vw] px-[5vw] leading-snug
@@ -45,31 +45,31 @@ export default function AboutSection() {
         >
           {displayText["about"][language]}
         </h1>
-        <p className="mb-[2vw] text-lg md:text-xl lg:text-2xl 2xl:text-3xl">
+        <p className="mb-[2vw] text-lg md:text-xl lg:text-2xl">
           Red Bridge Capital is a Hong Kong-based investment firm with a strong focus on China. We have built a diverse portfolio of investment vehicles and strategies, driven by a history of success in various sectors, reflecting our unwavering commitment to delivering outstanding results for our investors.
         </p>
         <h2 className="font-ssm uppercase leading-none mt-[2vw] mb-[2vw] text-2xl sm:text-3xl md:text-4xl">
           Our Approach
         </h2>
-        <p className="mb-[2vw] text-lg md:text-xl lg:text-2xl 2xl:text-3xl">
+        <p className="mb-[2vw] text-lg md:text-xl lg:text-2xl">
           Our roots in Hong Kong position us as a crucial bridge to China's thriving investment landscape. We are dedicated to identifying and seizing opportunities within China, leveraging our extensive network and deep understanding of the Chinese market to discover undervalued prospects that often go unnoticed.
         </p>
-        <p className="mb-[2vw] text-lg md:text-xl lg:text-2xl 2xl:text-3xl">
+        <p className="mb-[2vw] text-lg md:text-xl lg:text-2xl">
           We go beyond traditional investment management. We actively engage in the companies we invest in, drawing on our expertise and experiences, as well as the collaborative synergy with our investors, to foster mutual growth.
         </p>
         <h2 className="font-ssm uppercase leading-none mt-[2vw] mb-[2vw] text-2xl sm:text-3xl md:text-4xl">
           Bridging Connections
         </h2>
-        <p className="mb-[2vw] text-lg md:text-xl lg:text-2xl 2xl:text-3xl">
+        <p className="mb-[2vw] text-lg md:text-xl lg:text-2xl">
           More than just investment managers, we are bridge builders. Our leadership team's international background, rooted in Hong Kong, equips us to seamlessly facilitate companies' transition in and out of China, transcending barriers and fostering global connections.
         </p>
         <h2 className="font-ssm uppercase leading-none mt-[2vw] mb-[2vw] text-2xl sm:text-3xl md:text-4xl">
           Our Fundamental Values
         </h2>
-        <p className="mb-[2vw] text-lg md:text-xl lg:text-2xl 2xl:text-3xl">
+        <p className="mb-[2vw] text-lg md:text-xl lg:text-2xl">
           Integrity and transparency form the bedrock of our bridge, enabling us to connect opportunities, nurture  relationships, and navigate the intricacies of the financial landscape. These principles guide our work, ensuring the highest level of professionalism and reliability in all our endeavors.
         </p>
-        <p className="mb-[2vw] text-lg md:text-xl lg:text-2xl 2xl:text-3xl">
+        <p className="mb-[2vw] text-lg md:text-xl lg:text-2xl">
           As we progress, Red Bridge Capital remains steadfast in our commitment to excellence, partnership cultivation, and pioneering new horizons in the realm of investment.
         </p>
       </aside>
--- src/components/IntroSection/NavBar.jsx
+++ src/components/IntroSection/NavBar.jsx
@@ -24,11 +24,14 @@ const displayText = {
   }
 }
 
+const scrollToSection = (sectionId) => {
+  const section = document.getElementById(sectionId);
+  section?.scrollIntoView({ behavior: 'smooth' });
+};
+
 export default function NavBar() {
-  const { language, isChangingLanguage, cycleLanguage } = useContext(LanguageContext);
   const [ hoverLogo, setHoverLogo ] = useState(false);
   const [ bigmode, setBigmode ] = useState(window.matchMedia("(min-width: 640px)").matches);
-  const [ hoverToggle, setHoverToggle ] = useState(false);
   const [ menu, setMenu ] = useState(false);
 
   useEffect(() => {
@@ -36,11 +39,6 @@ export default function NavBar() {
       .matchMedia("(min-width: 640px)")
       .addEventListener('change', e => setBigmode( e.matches ));
   }, []);
-
-  const scrollTo = (sectionId) => {
-    const section = document.getElementById(sectionId);
-    section?.scrollIntoView({ behavior: 'smooth' });
-  };
 
   function toggleMenu() {
     window.scrollTo(0, 0);
@@ -50,11 +48,13 @@ export default function NavBar() {
   const showMenu = menu && !bigmode;
 
   return (
-    <nav id="nav"
+    <nav
+      id="nav"
       className={`absolute flex flex-col top-0 w-full z-30
       transition-all duration-300 ease-in-out ${showMenu ? 'bg-maroon h-screen !fixed' : 'h-20'}`}
     >
       <div
+        id="nav-top-bar"
         className="w-full flex justify-between p-[5vw] md:p-[2vw]"
       >
         <div
@@ -74,83 +74,109 @@ export default function NavBar() {
             style={{ opacity: hoverLogo ? 1 : 0 }}
           />
         </div>
-        {bigmode && <div
-          className="flex justify-between w-2/5 xl:w-1/5 text-maroon"
-        >
-          <button
-            className={`hover:text-beige duration-300 uppercase
-            ${isChangingLanguage ? 'opacity-0' : 'opacity-100'}`}
-            onClick={() => scrollTo("about-section")}
-          >
-            {displayText['about'][language]}
-          </button>
-          <button
-            className={`hover:text-beige duration-300 uppercase
-            ${isChangingLanguage ? 'opacity-0' : 'opacity-100'}`}
-            onClick={() => scrollTo("contact-section")}
-          >
-            {displayText['contact'][language]}
-          </button>
-          <button
-            className={`hover:text-beige duration-300 uppercase
-            ${isChangingLanguage ? 'opacity-0' : 'opacity-100'}`}
-            onClick={cycleLanguage}
-          >
-            {displayText['language'][language]}
-          </button>
-        </div>}
-        {!bigmode && <div
-            className="h-10 relative cursor-pointer"
-            onMouseLeave={() => setHoverToggle(false)}
-            onMouseEnter={() => setHoverToggle(true)}
-            onClick={toggleMenu}
-            >
-            <img src={navPlus}
-              className="h-10 transition-opacity duration-300 ease-in-out"
-              style={{ opacity: showMenu ? 0 : 1 }}
-            />
-            <img src={navMinus}
-              className="h-10 absolute top-0 transition-opacity duration-300 ease-in-out"
-              style={{ opacity: showMenu ? 1 : 0 }}
-            />
-        </div>}
+        {bigmode && <BigModeLinks />}
+        {!bigmode && <SmallModeToggle
+          showMenu={showMenu}
+          toggleMenu={toggleMenu}
+        />}
       </div>
-      {<div
-        className="flex flex-col justify-end p-8 transition-opacity duration-300 ease-in-out h-[90%]"
-        style={{ opacity: showMenu ? 1 : 0, visibility: showMenu ? "visible" : "hidden" }}
-      >
-          <div
-            className={`text-6xl text-beige cursor-pointer mb-16
-            hover:text-black duration-300 uppercase
-            ${isChangingLanguage ? 'opacity-0' : 'opacity-100'}`}
-            onClick={() => {
-              scrollTo("about-section");
-              setMenu(false);
-            }}
-          >
-            {displayText['about'][language]}
-          </div>
-          <div
-            className={`text-6xl text-beige cursor-pointer mb-16
-            hover:text-black duration-300 uppercase
-            ${isChangingLanguage ? 'opacity-0' : 'opacity-100'}`}
-            onClick={() => {
-              scrollTo("contact-section");
-              setMenu(false);
-            }}
-          >
-            {displayText['contact'][language]}
-          </div>
-          <div
-            className={`text-6xl text-beige cursor-pointer mb-16
-            hover:text-black duration-300 uppercase
-            ${isChangingLanguage ? 'opacity-0' : 'opacity-100'}`}
-            onClick={cycleLanguage}
-          >
-            {displayText['language'][language]}
-          </div>
-          <Clock className="text-4xl text-beige" />
-      </div>}
+      {!bigmode && <SmallModeMenu
+        showMenu={showMenu}
+        setMenu={setMenu}
+      />}
     </nav>
+  )
+}
+
+function BigModeLinks () {
+  const { language, isChangingLanguage, cycleLanguage } = useContext(LanguageContext);
+
+  return (
+    <div
+      className="flex justify-between w-2/5 xl:w-1/5 text-maroon"
+    >
+      <button
+        className={`hover:text-beige duration-300 uppercase
+        ${isChangingLanguage ? 'opacity-0' : 'opacity-100'}`}
+        onClick={() => scrollToSection("about-section")}
+      >
+        {displayText['about'][language]}
+      </button>
+      <button
+        className={`hover:text-beige duration-300 uppercase
+        ${isChangingLanguage ? 'opacity-0' : 'opacity-100'}`}
+        onClick={() => scrollToSection("contact-section")}
+      >
+        {displayText['contact'][language]}
+      </button>
+      <button
+        className={`hover:text-beige duration-300 uppercase
+        ${isChangingLanguage ? 'opacity-0' : 'opacity-100'}`}
+        onClick={cycleLanguage}
+      >
+        {displayText['language'][language]}
+      </button>
+    </div>
+  )
+}
+
+function SmallModeToggle ({ showMenu, toggleMenu }) {
+
+  return (
+    <div
+      className="h-10 relative cursor-pointer"
+      onClick={toggleMenu}
+    >
+    <img src={navPlus}
+      className="h-10 transition-opacity duration-300 ease-in-out"
+      style={{ opacity: showMenu ? 0 : 1 }}
+    />
+    <img src={navMinus}
+      className="h-10 absolute top-0 transition-opacity duration-300 ease-in-out"
+      style={{ opacity: showMenu ? 1 : 0 }}
+    />
+  </div>)
+}
+
+function SmallModeMenu ({ showMenu, setMenu }) {
+  const { language, isChangingLanguage, cycleLanguage } = useContext(LanguageContext);
+
+  return (
+    <div
+      className="flex flex-col justify-center p-8 transition-opacity duration-300 ease-in-out h-[90%]"
+      style={{ opacity: showMenu ? 1 : 0, visibility: showMenu ? "visible" : "hidden" }}
+    >
+      <div
+        className={`text-6xl text-beige cursor-pointer mb-16
+        hover:text-black duration-300 uppercase
+        ${isChangingLanguage ? 'opacity-0' : 'opacity-100'}`}
+        onClick={() => {
+          scrollToSection("about-section");
+          setMenu(false);
+        }}
+      >
+        {displayText['about'][language]}
+      </div>
+      <div
+        className={`text-6xl text-beige cursor-pointer mb-16
+        hover:text-black duration-300 uppercase
+        ${isChangingLanguage ? 'opacity-0' : 'opacity-100'}`}
+        onClick={() => {
+          scrollToSection("contact-section");
+          setMenu(false);
+        }}
+      >
+        {displayText['contact'][language]}
+      </div>
+      <div
+        className={`text-6xl text-beige cursor-pointer mb-16
+        hover:text-black duration-300 uppercase
+        ${isChangingLanguage ? 'opacity-0' : 'opacity-100'}`}
+        onClick={cycleLanguage}
+      >
+        {displayText['language'][language]}
+      </div>
+      <Clock className="text-4xl text-beige" />
+    </div>
   )
 }
